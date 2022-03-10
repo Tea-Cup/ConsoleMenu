0a314d30341798e6001a223471e25a2e3c734a93
Tea-Cup <cozyteacup@pm.me>
2022-03-10 18:28:51 +0700

    Split code into projects

---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleMenu;
+
+Console.WriteLine("test test");
+Menu menu = new("Test Menu") {
+	[1] = "Foo",
+	[2] = ("Bar", false),
+	[0] = "Baz",
+	[0] = "Xyz",
+	[3] = "Test"
+};
+int selected = menu.Show();
+bool bar = menu[2].IsChecked == true;
+Console.WriteLine($"Selected = [{selected}] {menu[selected].Text}");
+Console.WriteLine($"Bar = {(bar ? "true" : "false")}");
