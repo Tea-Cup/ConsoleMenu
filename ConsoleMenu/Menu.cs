e26e25e7f06156cbc4b487efe5267344352813ef
Tea-Cup <cozyteacup@pm.me>
2022-03-10 18:53:23 +0700

    documentation

--- ConsoleMenu/Menu.cs
+++ ConsoleMenu/Menu.cs
@@ -1,4 +1,5 @@
 ﻿namespace ConsoleMenu {
+	/// <summary>Menu container of <see cref="MenuItem"/>s</summary>
 	public class Menu : List<MenuItem> {
 		private static (ConsoleColor back, ConsoleColor fore) CurrentColor {
 			get => (Console.BackgroundColor, Console.ForegroundColor);
@@ -8,12 +9,19 @@
 			set => (Console.CursorLeft, Console.CursorTop) = value;
 		}
 
+		/// <summary>Optional text displayed at the top of menu</summary>
 		public string Title { get; set; } = "";
+		/// <summary>Background color of menu</summary>
 		public ConsoleColor BackColor { get; set; }
+		/// <summary>Foreground color of menu</summary>
 		public ConsoleColor ForeColor { get; set; }
 		private (ConsoleColor back, ConsoleColor fore) MainColor => (BackColor, ForeColor);
 		private (ConsoleColor back, ConsoleColor fore) RevsColor => (ForeColor, BackColor);
 
+		/// <summary>Indexer primarily made for object member initialization</summary>
+		/// <param name="id">ID of menu item. If <c>0</c>, then item is disabled</param>
+		/// <returns>First <see cref="MenuItem"/> with specified id</returns>
+		/// <exception cref="IndexOutOfRangeException">Thrown if no items found with specified id</exception>
 		public new MenuItem this[int id] {
 			set {
 				if (id != 0) {
@@ -26,27 +34,51 @@
 			get => this.FirstOrDefault(x => x.ID == id) ?? throw new IndexOutOfRangeException();
 		}
 
+		/// <summary>Creates a new instance of <see cref="Menu"/></summary>
 		public Menu() {
 			(BackColor, ForeColor) = CurrentColor;
 		}
+		/// <summary>Creates a new instance of <see cref="Menu"/> and changes its Title</summary>
+		/// <param name="title">Value for <see cref="Title"/> property</param>
 		public Menu(string title) : this() {
 			Title = title;
 		}
 
+		/// <summary>Constructs and adds a new instance of <see cref="MenuItem"/> with specified text</summary>
+		/// <param name="text">Text of a new <see cref="MenuItem"/></param>
+		/// <returns><see cref="MenuItem.ID"/> of a new item</returns>
 		public int Add(string text) {
 			return Add(0, text, true);
 		}
+		/// <summary>Constructs and adds a new instance of <see cref="MenuItem"/> with specified text</summary>
+		/// <param name="text">Text of a new <see cref="MenuItem"/></param>
+		/// <param name="enabled">Whether or not this item is enabled</param>
+		/// <returns><see cref="MenuItem.ID"/> of a new item</returns>
 		public int Add(string text, bool enabled) {
 			return Add(0, text, enabled);
 		}
+		/// <summary>Constructs and adds a new instance of <see cref="MenuItem"/> with specified text</summary>
+		/// <param name="id"><see cref="MenuItem.ID"/> of a new item</param>
+		/// <param name="text">Text of a new <see cref="MenuItem"/></param>
+		/// <returns><see cref="MenuItem.ID"/> of a new item</returns>
 		public int Add(int id, string text) {
 			return Add(id, text, true);
 		}
+		/// <summary>Constructs and adds a new instance of <see cref="MenuItem"/> with specified text</summary>
+		/// <param name="id"><see cref="MenuItem.ID"/> of a new item</param>
+		/// <param name="text">Text of a new <see cref="MenuItem"/></param>
+		/// <param name="enabled">Whether or not this item is enabled</param>
+		/// <returns><see cref="MenuItem.ID"/> of a new item</returns>
 		public int Add(int id, string text, bool enabled) {
 			MenuItem item = new(id, text, enabled);
 			Add(item);
 			return item.ID;
 		}
+
+		/// <summary>Replaces a <see cref="MenuItem"/> with specified id with another one.</summary>
+		/// <param name="id"><see cref="MenuItem.ID"/> of target item</param>
+		/// <param name="item">Replacement item</param>
+		/// <returns><see langword="true" /> if replacement occured.</returns>
 		public bool Set(int id, MenuItem item) {
 			if (id == 0) return false;
 			for (int i = 0; i < Count; ++i) {
@@ -59,6 +91,9 @@
 			return false;
 		}
 
+		/// <summary>Displays this menu and enters a loop until item is chosen</summary>
+		/// <param name="clear">If <see langword="true" />, then menu is erased from console afterwards</param>
+		/// <returns><see cref="MenuItem.ID"/> of selected item</returns>
 		public int Show(bool clear = true) {
 			if (Count == 0) return 0;
 			if (Console.CursorLeft > 0) Console.WriteLine();
--- ConsoleMenu/MenuItem.cs
+++ ConsoleMenu/MenuItem.cs
@@ -1,17 +1,52 @@
 ﻿namespace ConsoleMenu {
+	/// <summary>Container of a single item in <see cref="Menu"/></summary>
 	public class MenuItem : IEquatable<MenuItem> {
+		/// <summary>
+		/// <para>Numeric identifier of item.</para>
+		/// <para>Not an index. Uniqueness not guaranteed.</para>
+		/// </summary>
 		public int ID { get; set; }
+		/// <summary>Text displayed on item</summary>
 		public string Text { get; set; }
+		/// <summary>If <see langword="false" />, this item will not be selectable</summary>
 		public bool IsEnabled { get; set; }
+		/// <summary>
+		/// <para>Checkbox value of item.</para>
+		/// <para>If <see langword="null" />, thne this item does not have a checkbox</para>
+		/// </summary>
 		public bool? IsChecked { get; set; }
 
+		/// <summary>Constructs a new empty instance of <see cref="MenuItem"/></summary>
 		public MenuItem() : this("") { }
+		/// <summary>Constructs a new instance of <see cref="MenuItem"/></summary>
+		/// <param name="text">Text displayed on item</param>
 		public MenuItem(string text) : this(text, true) { }
+		/// <summary>Constructs a new instance of <see cref="MenuItem"/></summary>
+		/// <param name="text">Text displayed on item</param>
+		/// <param name="enabled">If <see langword="false" />, this item will not be selectable</param>
 		public MenuItem(string text, bool enabled) : this(0, text, enabled) { }
+		/// <summary>Constructs a new instance of <see cref="MenuItem"/></summary>
+		/// <param name="id">
+		/// <para>Numeric identifier of item.</para>
+		/// <para>Not an index. Uniqueness not guaranteed.</para>
+		/// </param>
+		/// <param name="text">Text displayed on item</param>
+		/// <param name="enabled">If <see langword="false" />, this item will not be selectable</param>
 		public MenuItem(int id, string text, bool enabled) {
 			(ID, Text, IsEnabled) = (id, text, enabled);
 		}
 
+		/// <summary></summary>
+		/// <param name="id">
+		/// <para>Numeric identifier of item.</para>
+		/// <para>Not an index. Uniqueness not guaranteed.</para>
+		/// </param>
+		/// <param name="text">Text displayed on item</param>
+		/// <param name="enabled">If <see langword="false" />, this item will not be selectable</param>
+		/// <param name="check">
+		/// <para>Checbox value of item.</para>
+		/// <para>If <see langword="null" />, thne this item does not have a checkbox</para>
+		/// </param>
 		public void Deconstruct(out int id, out string text, out bool enabled, out bool? check) {
 			id = ID;
 			text = Text;
@@ -19,25 +54,46 @@
 			check = IsChecked;
 		}
 
+		/// <summary>Determines whether the specified object is equal to the current object</summary>
+		/// <param name="other">The object to compare with the current object</param>
+		/// <returns><see langword="true" /> if the specified object is equal to the current object; otherwise, <see langword="false" /></returns>
 		public bool Equals(MenuItem? other) {
 			if (other is null) return false;
 			if (ReferenceEquals(this, other)) return true;
 			return ID == other.ID && Text == other.Text && IsEnabled == other.IsEnabled && IsChecked == other.IsChecked;
 		}
 
+		/// <summary>Determines whether the specified object is equal to the current object</summary>
+		/// <param name="obj">The object to compare with the current object</param>
+		/// <returns><see langword="true" /> if the specified object is equal to the current object; otherwise, <see langword="false" /></returns>
 		public override bool Equals(object? obj) {
 			return Equals(obj as MenuItem);
 		}
+		/// <summary>Calculates the hash code of this object</summary>
+		/// <returns>The calculated hash code</returns>
 		public override int GetHashCode() {
 			return HashCode.Combine(ID, Text, IsEnabled, IsChecked);
 		}
+		/// <summary>Determines whether the specified objects are equal</summary>
+		/// <param name="left">First object</param>
+		/// <param name="right">Second object</param>
+		/// <returns><see langword="true" /> if objects are equal; otherwise, <see langword="false" /></returns>
 		public static bool operator ==(MenuItem? left, MenuItem? right) {
 			return EqualityComparer<MenuItem>.Default.Equals(left, right);
 		}
+		/// <summary>Determines whether the specified objects are not equal</summary>
+		/// <param name="left">First object</param>
+		/// <param name="right">Second object</param>
+		/// <returns><see langword="true" /> if objects are not equal; otherwise, <see langword="false" /></returns>
 		public static bool operator !=(MenuItem? left, MenuItem? right) {
 			return !(left == right);
 		}
+
+		/// <summary>Constructs a new <see cref="MenuItem"/> from a <see cref="string"/></summary>
+		/// <param name="text">Text displayed on item</param>
 		public static implicit operator MenuItem(string text) => new(text);
+		/// <summary>Constructs a new <see cref="MenuItem"/> from a pair of <see cref="string"/> and <see cref="bool"/></summary>
+		/// <param name="value">Pair of <u>text displayed on item</u> and <u>checkbox value of item</u></param>
 		public static implicit operator MenuItem((string, bool) value) => new(value.Item1) { IsChecked = value.Item2 };
 	}
 }
